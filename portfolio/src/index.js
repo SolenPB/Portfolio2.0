dcae1b735241f245064fbe725cc1019842770bc5
SolenPB <solene.paratre@gmail.com>
2023-11-25 13:49:58 +0100

    maj

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <React>
+        <App/>
+    </React>
 );
